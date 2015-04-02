cf791e988106b94d1f3aeea12b5f9b0e526e5e8d
rpavlovic <rpavlovic@adage.com>
2015-04-02 06:53:16 -0400

    testing homepage content markup

--- wp-content/themes/enfold/template-home.php
+++ wp-content/themes/enfold/template-home.php
@@ -15,12 +15,12 @@
      ?>
 
 
-
-        <div class='container_wrap container_wrap_first main_color <?php avia_layout_class( 'main' ); ?>'>
 <!--
+        <div class='container_wrap container_wrap_first main_color <?php avia_layout_class( 'main' ); ?>'>
+
             <div class='container'>
 -->
-                <main id="main" role="main">
+                <main id="main" role="main" class="content <?php avia_layout_class( 'content' ); ?> units' <?php avia_markup_helper(array('context' => 'content'));?>">
 
                 <!-- <main class='template-archives content <?php avia_layout_class( 'content' ); ?> units' <?php avia_markup_helper(array('context' => 'content'));?>> -->
 
@@ -87,7 +87,7 @@
 
             <!-- </div>end container-->
 
-        </div><!-- close default .container_wrap element -->
+        <!-- </div>close default .container_wrap element -->
 
 
 <?php get_footer(); ?>
