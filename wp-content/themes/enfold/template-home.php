4a91bc60207a149180d1c3973eb746603ba5bebe
rpavlovic <rpavlovic@adage.com>
2015-04-02 06:48:04 -0400

    testing homepage content markup

--- wp-content/themes/enfold/template-home.php
+++ wp-content/themes/enfold/template-home.php
@@ -15,25 +15,54 @@
      ?>
 
 
-
+<!--
         <div class='container_wrap container_wrap_first main_color <?php avia_layout_class( 'main' ); ?>'>
 
             <div class='container'>
+-->
+                <main id="main" role="main">
 
-                <main class='template-archives content <?php avia_layout_class( 'content' ); ?> units' <?php avia_markup_helper(array('context' => 'content'));?>>
+                <!-- <main class='template-archives content <?php avia_layout_class( 'content' ); ?> units' <?php avia_markup_helper(array('context' => 'content'));?>> -->
 
-                    <div class="entry-content-wrapper entry-content clearfix">
+                    <!-- <div class="entry-content-wrapper entry-content clearfix"> -->
 [slideshow_deploy id='49']
-                        <section class="intro">
-                            <h1>we are THE BROOKLYN TRI CLUB.</h1>
-                            <p>
-                                <?php echo the_content(); ?>
-                            </p>
-                            <footer>
-                                <span>Is the Brooklyn Tri Club right for you?</span>
-                                <a href="#" class="btn-join">JOIN US</a>
-                            </footer>
-                        </section>
+                    <section class="intro">
+                        <h1>we are THE BROOKLYN TRI CLUB.</h1>
+                        <p>
+                            <?php echo the_content(); ?>
+                        </p>
+                        <footer>
+                            <span>Is the Brooklyn Tri Club right for you?</span>
+                            <a href="/register" class="btn-join">JOIN US</a>
+                        </footer>
+                    </section>
+                    <section class="box-area">
+                        <div class="holder">
+                            <div class="frame">
+                                <div class="col">
+                                    <div class="col-holder">
+                                        <div class="icon-holder"><i class="icon-star">&nbsp;</i></div>
+                                        <h2>our mission</h2>
+                                        <p>BTC aims to make triathlon accessible and affordable for everyone. <a href="#">Learn more</a> about the club.</p>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="col-holder">
+                                        <div class="icon-holder picton-blue"><i class="icon-star">&nbsp;</i></div>
+                                        <h2>member perks</h2>
+                                        <p>We enjoy <a href="#">member discounts</a> on products, services and race entries with dozens of companies, here in Brooklyn and online.</p>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="col-holder">
+                                        <div class="icon-holder mantis"><i class="icon-star">&nbsp;</i></div>
+                                        <h2>faq<span class="lowercase">s</span></h2>
+                                        <p>BTC aims to make triathlon accessible and affordable for everyone. <a href="#">Learn more</a> about the club.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
 
                     <?php
                     //display the actual post content
@@ -43,7 +72,7 @@
                     ?>
 
 
-                    </div>
+                    <!-- </div> -->
 
                 <!--end content-->
                 </main>
@@ -55,10 +84,6 @@
                 get_sidebar();
 
                 ?>
-
-            </div><!--end container-->
-
-        </div><!-- close default .container_wrap element -->
 
 
 <?php get_footer(); ?>
