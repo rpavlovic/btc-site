a27a2215a112092e5a4c4f889e99a2229b4facba
rpavlovic <rpavlovic@adage.com>
2015-04-10 10:31:44 -0400

    testing new theme

--- wp-content/themes/brooklyntri/header.php
+++ wp-content/themes/brooklyntri/header.php
@@ -15,13 +15,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,600,700' rel='stylesheet' type='text/css'>
-	<link media="all" rel="stylesheet" href="css/all.css">
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 	<![endif]-->
 	<script>(function(){document.documentElement.className='js'})();</script>
 	<?php wp_head(); ?>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,600,700' rel='stylesheet' type='text/css'>
+	<link media="all" rel="stylesheet" href="css/all.css">
 </head>
 
 <body>
