3d84681ae3c288ac9fecd96e169aadf578466cbe
rpavlovic <rpavlovic@adage.com>
2015-04-14 14:17:12 -0400

    added contact form stuff

--- wp-content/themes/brooklyntri/contact.php
+++ wp-content/themes/brooklyntri/contact.php
@@ -25,12 +25,25 @@ get_header(); ?>
 						<h1>RESOURCES</h1>
 					</div>
 				</section>
-				<div id="two-columns">
-					<div id="content">
+				<div class="contact-form-area">
+
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			// Include the page content template.
+			get_template_part( 'content', 'contact' ); //content-contact.php
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		// End the loop.
+		endwhile;
+		?>
 
 
-					</div>
-					<?php btc_leftnav( $post ); ?>
 				</div>
 
 <?php	
---
+++ wp-content/themes/brooklyntri/content-contact.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The template used for displaying page content
+ *
+ * @package WordPress
+ * @subpackage Brooklyn_Tri
+ * @since Brooklyn Tri 1.0
+ */
+
+?>
+
+						<div class="img-holder">
+							<img src="images/img14.jpg" alt="image description">
+						</div>
+						<div class="text">
+                            <?php the_title('<h2>','</h2>'); ?>
+                            <?php the_content(); ?>
+						</div>
+					</div>
+					<form action="#" class="contact-form">
+						<fieldset>
+							<legend class="hidden">contact form</legend>
+							<div class="heading"><h2>SEND US AN EMAIL!</h2></div>
+							<div class="row">
+								<div class="col">
+									<label for="name">NAME <span>*</span></label>
+									<input type="text" id="name">
+								</div>
+								<div class="col">
+									<label for="email">E-MAIL <span>*</span></label>
+									<input type="email" id="email">
+								</div>
+							</div>
+							<div class="row">
+								<label for="subject">SUBJECT <span>*</span></label>
+								<input type="text" id="subject">
+							</div>
+							<div class="row">
+								<label for="message">MESSAGE <span>*</span></label>
+								<textarea id="message"></textarea>
+							</div>
+							<input class="button" type="submit" value="SUBMIT">
+						</fieldset>
+					</form>
+
+
