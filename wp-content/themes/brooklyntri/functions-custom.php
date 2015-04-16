13394e945ee9652eb816b8230472e119086b3d40
rpavlovic <rpavlovic@adage.com>
2015-04-16 18:45:32 -0400

    latest changes

--- wp-content/themes/brooklyntri/functions-custom.php
+++ wp-content/themes/brooklyntri/functions-custom.php
@@ -3,26 +3,49 @@
 /**
  * login capture
  */
-add_action('init', function(){
 
-	// not the login request?
-	if( !isset( $_POST['action'] ) || $_POST['action'] !== 'btc_login_jam') {
-		// redirect back to the requested page
-		return;
-	}
+function btc_login() {
 
-	// see the codex for wp_signon()
-	$result = wp_signon();
+    // not the login request?
+    if( !isset( $_POST['action'] ) || $_POST['action'] !== 'btc_login_jam') {
+        return;
+    }
 
-	if ( is_wp_error( $result ) ) {
-		wp_die('Login failed. Wrong password or user name?');
-	}
-	
+    if( isset( $_POST['action'] ) && $_POST['action'] === 'btc_login_jam') {
 
-	// redirect back to the requested page if login was successful    
-	header('Location: ' . $_SERVER['REQUEST_URI']);
-	exit;
-});
+        global $wpdb;
+
+        //We shall SQL escape all inputs
+        $username = $wpdb->escape( $_POST['user'] );
+        $password = $wpdb->escape( $_POST['pass'] );
+        //$remember = $wpdb->escape($_POST['rememberme']);
+
+        //if($remember) $remember = "true";
+        //else $remember = "false";
+
+        $login_data = array();
+        $login_data['user_login'] = $username;
+        $login_data['user_password'] = $password;
+        $login_data['remember'] = false; //$remember;
+
+        $user_verify = wp_signon( $login_data, false ); 
+
+        if ( is_wp_error( $user_verifyv) )  {
+            // error message
+            echo $user_verify->get_error_message();
+        }
+        // redirect back to the requested page if login was successful
+        echo $_SERVER['REQUEST_URI'];
+        die;  
+        header('Location: ' . $_SERVER['REQUEST_URI']);
+        exit;
+    }
+    else {
+        // No login details entered - you should probably add some more user feedback here, but this does the bare minimum
+        echo "Invalid login details";
+    }
+}
+add_action( 'after_setup_theme', 'btc_login' );
 
 // lose the admin bar
 add_filter('show_admin_bar', '__return_false');
--- wp-content/themes/brooklyntri/login.php
+++ wp-content/themes/brooklyntri/login.php
@@ -1,34 +1,20 @@
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
-
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
-
+/*
+Template Name: Login
+*/
 
 //get_header();
-die(1);
+
 function btc_login() {
 
     // not the login request?
     if( !isset( $_POST['action'] ) || $_POST['action'] !== 'btc_login_jam') {
-        die(2);
         return;
     }
 
     if( isset( $_POST['action'] ) && $_POST['action'] === 'btc_login_jam') {
-die(3);
+
         global $wpdb;
 
         //We shall SQL escape all inputs
@@ -48,17 +34,15 @@ die(3);
 
         if ( is_wp_error( $user_verifyv) )  {
             // error message
-            die(4);
             echo $user_verify->get_error_message();
         }
         // redirect back to the requested page if login was successful
-        //echo $_SERVER['REQUEST_URI']; die;
-        die(5);
+        echo $_SERVER['REQUEST_URI'];
+        die;  
         header('Location: ' . $_SERVER['REQUEST_URI']);
         exit;
     }
     else {
-        die(6);
         // No login details entered - you should probably add some more user feedback here, but this does the bare minimum
         echo "Invalid login details";
     }
