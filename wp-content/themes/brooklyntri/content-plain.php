a4b931ba4cac02fa6e336ec951d6ad80f845fad4
rpavlovic <rpavlovic@adage.com>
2015-04-16 19:21:59 -0400

    added plain type template

---
+++ wp-content/themes/brooklyntri/content-plain.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template used for displaying page content
+ *
+ * @package WordPress
+ * @subpackage Brooklyn_Tri
+ * @since Brooklyn Tri 1.0
+ */
+
+?>
+
+				<div class="intro-text">
+
+                    <?php the_title('<h2>','</h2>'); ?>
+
+                    <?php the_content(); ?>
+
+                </div>
