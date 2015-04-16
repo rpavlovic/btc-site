de5e47405c8f056c2c674d21e3d423d1dbd975af
rpavlovic <rpavlovic@adage.com>
2015-04-16 18:40:46 -0400

    latest changes

--- wp-content/themes/brooklyntri/login.php
+++ wp-content/themes/brooklyntri/login.php
@@ -5,16 +5,17 @@ Template Name: Login
 */
 
 //get_header();
-
+die(1);
 function btc_login() {
 
     // not the login request?
     if( !isset( $_POST['action'] ) || $_POST['action'] !== 'btc_login_jam') {
+        die(2);
         return;
     }
 
     if( isset( $_POST['action'] ) && $_POST['action'] === 'btc_login_jam') {
-
+die(3);
         global $wpdb;
 
         //We shall SQL escape all inputs
@@ -34,15 +35,17 @@ function btc_login() {
 
         if ( is_wp_error( $user_verifyv) )  {
             // error message
+            die(4);
             echo $user_verify->get_error_message();
         }
         // redirect back to the requested page if login was successful
-        echo $_SERVER['REQUEST_URI'];
-        die;  
+        //echo $_SERVER['REQUEST_URI']; die;
+        die(5);
         header('Location: ' . $_SERVER['REQUEST_URI']);
         exit;
     }
     else {
+        die(6);
         // No login details entered - you should probably add some more user feedback here, but this does the bare minimum
         echo "Invalid login details";
     }
