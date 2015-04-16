c397d5f505998df5b3c599860f970d0b2a0f39cf
rpavlovic <rpavlovic@adage.com>
2015-04-16 08:55:28 -0400

    latest changes

--- wp-content/themes/brooklyntri/login.php
+++ wp-content/themes/brooklyntri/login.php
@@ -6,45 +6,47 @@ Template Name: Login
 
 get_header();
 
-// not the login request?
-if( !isset( $_POST['action'] ) || $_POST['action'] !== 'btc_login_jam') {
-    return;
-}
+function btc_login() {
 
-if( isset( $_POST['action'] ) && $_POST['action'] === 'btc_login_jam') {
- 
-    global $wpdb;
- 
-    //We shall SQL escape all inputs
-    $username = $wpdb->escape( $_POST['user'] );
-    $password = $wpdb->escape( $_POST['pass'] );
-    //$remember = $wpdb->escape($_POST['rememberme']);
- 
-    //if($remember) $remember = "true";
-    //else $remember = "false";
- 
-    $login_data = array();
-    $login_data['user_login'] = $username;
-    $login_data['user_password'] = $password;
-    $login_data['remember'] = false; //$remember;
- 
-    $user_verify = wp_signon( $login_data, false ); 
- 
-    if ( is_wp_error( $user_verify) )  {
-        // error message
-        wp_die( 'oh, snap' );
+    // not the login request?
+    if( !isset( $_POST['action'] ) || $_POST['action'] !== 'btc_login_jam') {
+        return;
     }
-    // redirect back to the requested page if login was successful    
-    header('Location: ' . $_SERVER['REQUEST_URI']);
-    exit;
- 
+
+    if( isset( $_POST['action'] ) && $_POST['action'] === 'btc_login_jam') {
+
+        global $wpdb;
+
+        //We shall SQL escape all inputs
+        $username = $wpdb->escape( $_POST['user'] );
+        $password = $wpdb->escape( $_POST['pass'] );
+        //$remember = $wpdb->escape($_POST['rememberme']);
+
+        //if($remember) $remember = "true";
+        //else $remember = "false";
+
+        $login_data = array();
+        $login_data['user_login'] = $username;
+        $login_data['user_password'] = $password;
+        $login_data['remember'] = false; //$remember;
+
+     echo 'here';
+        $user_verify = wp_signon( $login_data, false ); 
+     echo 'there';
+
+        if ( is_wp_error( $user_verifyv) )  {
+            // error message
+            echo $user_verify->get_error_message();
+        }
+        // redirect back to the requested page if login was successful    
+        header('Location: ' . $_SERVER['REQUEST_URI']);
+        exit;
+    }
+    else {
+        // No login details entered - you should probably add some more user feedback here, but this does the bare minimum
+        echo "Invalid login details";
+    }
 }
-else {
- 
-    // No login details entered - you should probably add some more user feedback here, but this does the bare minimum
- 
-    echo "Invalid login details";
- 
-}
+add_action( 'after_setup_theme', 'btc_login' );
 
 get_footer();
