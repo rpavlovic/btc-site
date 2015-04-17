8227e78ba1f97c2dbcc2f30d2cf6c1285c7149f4
rpavlovic <rpavlovic@adage.com>
2015-04-17 08:07:29 -0400

    latest changes

--- wp-content/themes/brooklyntri/board.php
+++ wp-content/themes/brooklyntri/board.php
@@ -25,7 +25,7 @@ btc_breadcrumbs();
 		while ( have_posts() ) : the_post();
 
 			// Include the page content template.
-			get_template_part( 'content', 'about' ); //content-about.php
+			get_template_part( 'content', 'board' ); //content-board.php
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
