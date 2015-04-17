c0da6a1789540ee248fbb64159a9ef02096cae05
rpavlovic <rpavlovic@adage.com>
2015-04-17 08:06:00 -0400

    latest changes

--- wp-content/themes/brooklyntri/content-board.php
+++ wp-content/themes/brooklyntri/content-board.php
@@ -25,6 +25,7 @@
 							<div class="holder">
 <?php
     $board = get_posts( array( 'category' => 6, 'orderby' => 'post_date', 'order' => 'DESC' ) );
+    var_dump($board);
     foreach ( $board as $member ): setup_postdata( $member );
 		$image = wp_get_attachment_url( get_post_thumbnail_id( $member->ID ) );
         $content = get_fields( $post->ID );
