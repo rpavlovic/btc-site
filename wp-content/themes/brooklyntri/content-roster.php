4cfd08105a6308c7aa85a033bc8efcb743850ea7
rpavlovic <rpavlovic@adage.com>
2015-04-17 06:24:23 -0400

    latest changes

---
+++ wp-content/themes/brooklyntri/content-roster.php
@@ -0,0 +1,285 @@
+<?php
+/**
+ * The template used for displaying page content
+ *
+ * @package WordPress
+ * @subpackage Brooklyn_Tri
+ * @since Brooklyn Tri 1.0
+ */
+
+?>
+
+					<header>
+
+                            <?php the_title('<h2>','</h2>'); ?>
+
+                            <?php the_content(); ?>
+
+					</header>
+					<div class="bar">
+						<p><i class="icon-star"></i> DENOTES 2015 CLUB RACE</p>
+					</div>
+					<form action="#" class="info-form">
+						<fieldset>
+							<legend class="hidden">info form</legend>
+							<ul class="info-list">
+								<li class="active">
+									<input type="checkbox" title="checkbox" checked>
+									<a class="opener" href="#">
+										<div class="col">
+											<h2><i class="icon-star"></i> BROOKLYN HALF MARATHON</h2>
+										</div>
+										<div class="col">
+											<span>50 BTCERS</span>
+										</div>
+										<div class="col">
+											<time datetime="2015-05-15">05/16/15</time>
+										</div>
+									</a>
+									<div class="slide">
+										<dl>
+											<dt>TYPE:</dt>
+											<dd>Half Marathon</dd>
+											<dt>LOCATION:</dt>
+											<dd>Brooklyn, NY</dd>
+										</dl>
+										<dl>
+											<dt>WEBSITE:</dt>
+											<dd><a href="#">www.nyrr.org/races-and-events/2015/...</a></dd>
+											<dt>ATHLETE LIST:</dt>
+											<dd><a href="#">click to view</a></dd>
+										</dl>
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" title="checkbox">
+									<a class="opener" href="#">
+										<div class="col">
+											<h2>FLAT AS A PANCAKE</h2>
+										</div>
+										<div class="col">
+											<span>10 BTCERS</span>
+										</div>
+										<div class="col">
+											<time datetime="2015-06-13">06/13/15</time>
+										</div>
+									</a>
+									<div class="slide">
+										<dl>
+											<dt>TYPE:</dt>
+											<dd>Half Marathon</dd>
+											<dt>LOCATION:</dt>
+											<dd>Brooklyn, NY</dd>
+										</dl>
+										<dl>
+											<dt>WEBSITE:</dt>
+											<dd><a href="#">www.nyrr.org/races-and-events/2015/...</a></dd>
+											<dt>ATHLETE LIST:</dt>
+											<dd><a href="#">click to view</a></dd>
+										</dl>
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" title="checkbox">
+									<a class="opener" href="#">
+										<div class="col">
+											<h2><i class="icon-star"></i> NYC TRIATHLON</h2>
+										</div>
+										<div class="col">
+											<span>6 BTCERS</span>
+										</div>
+										<div class="col">
+											<time datetime="2015-07-19">07/19/15</time>
+										</div>
+									</a>
+									<div class="slide">
+										<dl>
+											<dt>TYPE:</dt>
+											<dd>Half Marathon</dd>
+											<dt>LOCATION:</dt>
+											<dd>Brooklyn, NY</dd>
+										</dl>
+										<dl>
+											<dt>WEBSITE:</dt>
+											<dd><a href="#">www.nyrr.org/races-and-events/2015/...</a></dd>
+											<dt>ATHLETE LIST:</dt>
+											<dd><a href="#">click to view</a></dd>
+										</dl>
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" title="checkbox">
+									<a class="opener" href="#">
+										<div class="col">
+											<h2>IRONMAN LAKE PLACID</h2>
+										</div>
+										<div class="col">
+											<span>10 BTCERS</span>
+										</div>
+										<div class="col">
+											<time datetime="2015-07-26">07/26/15</time>
+										</div>
+									</a>
+									<div class="slide">
+										<dl>
+											<dt>TYPE:</dt>
+											<dd>Half Marathon</dd>
+											<dt>LOCATION:</dt>
+											<dd>Brooklyn, NY</dd>
+										</dl>
+										<dl>
+											<dt>WEBSITE:</dt>
+											<dd><a href="#">www.nyrr.org/races-and-events/2015/...</a></dd>
+											<dt>ATHLETE LIST:</dt>
+											<dd><a href="#">click to view</a></dd>
+										</dl>
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" title="checkbox">
+									<a class="opener" href="#">
+										<div class="col">
+											<h2>FLAT AS A PANCAKE</h2>
+										</div>
+										<div class="col">
+											<span>10 BTCERS</span>
+										</div>
+										<div class="col">
+											<time datetime="2015-06-13">06/13/15</time>
+										</div>
+									</a>
+									<div class="slide">
+										<dl>
+											<dt>TYPE:</dt>
+											<dd>Half Marathon</dd>
+											<dt>LOCATION:</dt>
+											<dd>Brooklyn, NY</dd>
+										</dl>
+										<dl>
+											<dt>WEBSITE:</dt>
+											<dd><a href="#">www.nyrr.org/races-and-events/2015/...</a></dd>
+											<dt>ATHLETE LIST:</dt>
+											<dd><a href="#">click to view</a></dd>
+										</dl>
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" title="checkbox">
+									<a class="opener" href="#">
+										<div class="col">
+											<h2>NYC TRIATHLON</h2>
+										</div>
+										<div class="col">
+											<span>6 BTCERS</span>
+										</div>
+										<div class="col">
+											<time datetime="2015-07-19">07/19/15</time>
+										</div>
+									</a>
+									<div class="slide">
+										<dl>
+											<dt>TYPE:</dt>
+											<dd>Half Marathon</dd>
+											<dt>LOCATION:</dt>
+											<dd>Brooklyn, NY</dd>
+										</dl>
+										<dl>
+											<dt>WEBSITE:</dt>
+											<dd><a href="#">www.nyrr.org/races-and-events/2015/...</a></dd>
+											<dt>ATHLETE LIST:</dt>
+											<dd><a href="#">click to view</a></dd>
+										</dl>
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" title="checkbox">
+									<a class="opener" href="#">
+										<div class="col">
+											<h2>IRONMAN LAKE PLACID</h2>
+										</div>
+										<div class="col">
+											<span>10 BTCERS</span>
+										</div>
+										<div class="col">
+											<time datetime="2015-07-26">07/26/15</time>
+										</div>
+									</a>
+									<div class="slide">
+										<dl>
+											<dt>TYPE:</dt>
+											<dd>Half Marathon</dd>
+											<dt>LOCATION:</dt>
+											<dd>Brooklyn, NY</dd>
+										</dl>
+										<dl>
+											<dt>WEBSITE:</dt>
+											<dd><a href="#">www.nyrr.org/races-and-events/2015/...</a></dd>
+											<dt>ATHLETE LIST:</dt>
+											<dd><a href="#">click to view</a></dd>
+										</dl>
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" title="checkbox">
+									<a class="opener" href="#">
+										<div class="col">
+											<h2>NYC TRIATHLON</h2>
+										</div>
+										<div class="col">
+											<span>6 BTCERS</span>
+										</div>
+										<div class="col">
+											<time datetime="2015-07-19">07/19/15</time>
+										</div>
+									</a>
+									<div class="slide">
+										<dl>
+											<dt>TYPE:</dt>
+											<dd>Half Marathon</dd>
+											<dt>LOCATION:</dt>
+											<dd>Brooklyn, NY</dd>
+										</dl>
+										<dl>
+											<dt>WEBSITE:</dt>
+											<dd><a href="#">www.nyrr.org/races-and-events/2015/...</a></dd>
+											<dt>ATHLETE LIST:</dt>
+											<dd><a href="#">click to view</a></dd>
+										</dl>
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" title="checkbox">
+									<a class="opener" href="#">
+										<div class="col">
+											<h2>IRONMAN LAKE PLACID</h2>
+										</div>
+										<div class="col">
+											<span>10 BTCERS</span>
+										</div>
+										<div class="col">
+											<time datetime="2015-07-26">07/26/15</time>
+										</div>
+									</a>
+									<div class="slide">
+										<dl>
+											<dt>TYPE:</dt>
+											<dd>Half Marathon</dd>
+											<dt>LOCATION:</dt>
+											<dd>Brooklyn, NY</dd>
+										</dl>
+										<dl>
+											<dt>WEBSITE:</dt>
+											<dd><a href="#">www.nyrr.org/races-and-events/2015/...</a></dd>
+											<dt>ATHLETE LIST:</dt>
+											<dd><a href="#">click to view</a></dd>
+										</dl>
+									</div>
+								</li>
+							</ul>
+							<div class="btn-holder">
+								<input type="submit" class="btn" value="SAVE CHANGES">
+								<a class="btn" href="#">SUGGEST A RACE</a>
+							</div>
+						</fieldset>
+					</form>
+
---
+++ wp-content/themes/brooklyntri/roster.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Template Name: Race Roster
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Brooklyn_Tri
+ * @since Brooklyn Tri 1.0
+ */
+
+get_header();
+
+btc_breadcrumbs();
+
+?>
+
+
+				<section class="info-holder">
+
+
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			// Include the page content template.
+			get_template_part( 'content', 'roster' ); //content-roster.php
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		// End the loop.
+		endwhile;
+		?>
+
+				</section>
+
+<?php	
+	btc_get_sponsor_logos();
+	get_footer();
+?>
