57c94a94877e538a5a0ba6f976f51613ab6e3c6c
Rahmin Pavlovic <rpavlovic@northpointdigital.com>
2015-06-03 16:43:54 -0400

    latest

--- wp-content/themes/brooklyntri/functions-custom.php
+++ wp-content/themes/brooklyntri/functions-custom.php
@@ -1054,14 +1054,14 @@ function add_user_jawn( $form ) {
 
 	$u = get_userdata( get_current_user_id() );
 	foreach( $form['fields'] as &$field ) {
-		var_dump($field);
-		if ( $field->id == 1 ) {
+		//var_dump($field);
+		if ( $field->id == '1' ) {
 			$field->content = $u->first_name;
 		}
-		if ( $field->id == 2 ) {
+		if ( $field->id == '2' ) {
 			$field->content = $u->last_name;
 		}
-		if ( $field->id == 3 ) {
+		if ( $field->id == '3' ) {
 			$field->content = $u->email;
 		}
 	}
