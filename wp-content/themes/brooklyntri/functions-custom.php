a7cb083860e091c91b7f6b2be0b9eb3730f0511a
Rahmin Pavlovic <rpavlovic@northpointdigital.com>
2015-06-08 15:22:05 -0400

    latest

--- wp-content/themes/brooklyntri/functions-custom.php
+++ wp-content/themes/brooklyntri/functions-custom.php
@@ -974,6 +974,7 @@ function get_btc_participants($form_id, $event_id) {
 	$registrants = RGFormsModel::get_leads($form_id, '2', 'ASC');
 	$racers = array();
 	foreach ($registrants as $racer) {
+		var_dump($racer);
 		if($racer[EVENT_FIELD_ID] == $event_id) {
 			$racers[] = $racer['1'] . ' ' .$racer['2'];
 		}
