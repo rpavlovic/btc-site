87eceef797bcb4bac92eb320438c499d6bbbe7f7
rpavlovic <rpavlovic@adage.com>
2015-08-08 06:22:21 -0400

    latest

--- wp-content/themes/brooklyntri/content-forum.php
+++ wp-content/themes/brooklyntri/content-forum.php
@@ -7,11 +7,13 @@
  * @since Brooklyn Tri 1.0
  */
 
+$infobox = get_fields( get_the_ID() );
+
 ?>
 				<section class="forum-area">
 					<header>
 						<?php the_title('<h1>','</h1>'); ?>
-						
+						<?php echo esc_html( infobox['intro'] ); ?>
 					</header>
 
 					<div class="divider">
@@ -31,7 +33,7 @@
 
 				    <div class="post" id="post-<?php the_ID(); ?>">
 				        <div class="entry forum-table">
-<?= the_content(); ?>
+							<?= the_content(); ?>
 				        </div>
 				    </div>
 
