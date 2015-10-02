1248dc25850d5f687f6df500d098bc7d7f35e1e6
rpavlovic <rpavlovic@adage.com>
2015-10-01 20:31:24 -0400

    latest

--- wp-content/themes/brooklyntri/header.php
+++ wp-content/themes/brooklyntri/header.php
@@ -25,6 +25,29 @@
 </head>
 
 <body>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1609626645924704',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+<div
+  class="fb-like"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true">
+</div>
 	<noscript>Javascript must be enabled for the correct page display</noscript>
 	<div id="wrapper">
 		<div class="w1">
