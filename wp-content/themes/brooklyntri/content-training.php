dcb4377e989ff66896c003678e4eedde71b9108c
rpavlovic <rpavlovic@adage.com>
2015-10-03 14:07:45 -0400

    latest

--- wp-content/themes/brooklyntri/content-training.php
+++ wp-content/themes/brooklyntri/content-training.php
@@ -10,7 +10,6 @@ $fields = get_fields( get_the_ID() );
 
 ?>
 
-
                         <div class="intro-text">
 
                             <?php the_title('<h2>','</h2>'); ?>
@@ -24,11 +23,11 @@ $fields = get_fields( get_the_ID() );
 
 <?php if ( is_user_logged_in() ): ?>
 							<p>
-								<?= $fields['logged_in_text'] ?>
+								<?= $fields['logged-in_text'] ?>
 							</p>
 <?php else: ?>
 							<p>
-								<?= $fields['logged_out_text'] ?>
+								<?= $fields['logged-out_text'] ?>
 							</p>
 <?php endif; ?>
 
