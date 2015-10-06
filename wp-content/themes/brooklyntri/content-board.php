9f6c827c7976ea211ddac2080d476cb31a9c3e27
rpavlovic <rpavlovic@adage.com>
2015-10-06 06:33:16 -0400

    latest

--- wp-content/themes/brooklyntri/content-board.php
+++ wp-content/themes/brooklyntri/content-board.php
@@ -48,7 +48,7 @@ foreach ($categories as $cat => $slug):
 		$image = wp_get_attachment_url( get_post_thumbnail_id( $member->ID ) );
         $content = get_fields( $member->ID );
 
-        if ( !in_array($member->ID, $shown) ):
+        if ( true || !in_array($member->ID, $shown) ):
         	$shown[] = $member->ID;
 ?>
 								<article class="info" itemscope itemtype="http://schema.org/Person">
