0620b21cc276883f94ad4afcd318cbc603d63035
rpavlovic <rpavlovic@adage.com>
2015-10-22 15:10:34 -0400

    latest

--- wp-content/themes/brooklyntri/forum.php
+++ wp-content/themes/brooklyntri/forum.php
@@ -18,8 +18,8 @@ btc_breadcrumbs();
 ?>
 
 				<div id="two-columns">
-					<?php forum_leftnav(); ?>
 					<div id="content">
+					<?php forum_leftnav(); ?>
 <?
 // Start the loop.
 while ( have_posts() ) : the_post();
