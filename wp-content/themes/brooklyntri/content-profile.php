22988652aa550a674f0aef0f91fb3e28bb243765
rpavlovic <rpavlovic@adage.com>
2015-11-01 07:27:40 -0500

    latest

---
+++ wp-content/themes/brooklyntri/content-profile.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template used for displaying profile content
+ *
+ * @package WordPress
+ * @subpackage Brooklyn_Tri
+ * @since Brooklyn Tri 1.0
+ */
+
+
+?>
+
+					<div class="holder">
+						<div class="img-holder">
+							<img src="images/img14.jpg" alt="image description">
+						</div>
+						<div class="text">
+
+		                    <?php the_title('<h2>','</h2>'); ?>
+
+		                    <?php the_content(); ?>
+
+						</div>
+					</div>
+					<form action="#" class="contact-form">
+						<fieldset>
+							<legend class="hidden">contact form</legend>
+							<div class="heading"><h2>EDIT YOUR PROFILE</h2></div>
+							<div class="row">
+								<div class="col">
+									<label for="first_name">FIRST NAME <span>*</span></label>
+									<input type="text" name="first_name" id="first_name" value="Rahmin">
+								</div>
+								<div class="col">
+									<label for="first_name">FIRST NAME <span>*</span></label>
+									<input type="text" name="last_name" id="last_name" value="Rahmin">
+								</div>
+							</div>
+							<div class="row">
+								<label for="nickname">NICKNAME <span>*</span></label>
+								<input type="text" name="nickname" id="nickname" value="Rahmin">
+							</div>
+							<div class="row">
+								<label for="email">E-MAIL <span>*</span></label>
+								<input type="text" name="email" id="email" value="Rahmin">
+							</div>
+							<div class="row">
+								<label for="email">WEBSITE <span>*</span></label>
+								<input type="text" name="website" id="website" value="Rahmin">
+							</div>
+							<input class="button" type="submit" value="SUBMIT">
+						</fieldset>
+					</form>
--- wp-content/themes/brooklyntri/profile.php
+++ wp-content/themes/brooklyntri/profile.php
@@ -1,5 +1,11 @@
 <?php
 /**
+ * Template Name: Profile
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
  * @package WordPress
  * @subpackage Brooklyn_Tri
  * @since Brooklyn Tri 1.0
@@ -12,46 +18,27 @@ btc_breadcrumbs();
 //$current_post = $post;
 
 ?>
+
 				<div class="contact-form-area">
-					<div class="holder">
-						<div class="img-holder">
-							<img src="images/img14.jpg" alt="image description">
-						</div>
-						<div class="text">
-							<h1>YOUR PROFILE</h1>
-							<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae quam eu odio feugiat ullamcorper. Nam aliquam dignissim mattis. Donec scelerisque congue eros id vehicula. Quisque quis lorem in nunc facilisis maximus in a lorem. Suspendisse nec sagittis lacus, sed pharetra neque. Vivamus fringilla ultricies sapien, molestie efficitur erat gravida non.</p>
-						</div>
-					</div>
-					<form action="#" class="contact-form">
-						<fieldset>
-							<legend class="hidden">contact form</legend>
-							<div class="heading"><h2>EDIT YOUR PROFILE</h2></div>
-							<div class="row">
-								<div class="col">
-									<label for="first_name">FIRST NAME <span>*</span></label>
-									<input type="text" name="first_name" id="first_name" value="Rahmin">
-								</div>
-								<div class="col">
-									<label for="first_name">FIRST NAME <span>*</span></label>
-									<input type="text" name="last_name" id="last_name" value="Rahmin">
-								</div>
-							</div>
-							<div class="row">
-								<label for="nickname">NICKNAME <span>*</span></label>
-								<input type="text" name="nickname" id="nickname" value="Rahmin">
-							</div>
-							<div class="row">
-								<label for="email">E-MAIL <span>*</span></label>
-								<input type="text" name="email" id="email" value="Rahmin">
-							</div>
-							<div class="row">
-								<label for="email">WEBSITE <span>*</span></label>
-								<input type="text" name="website" id="website" value="Rahmin">
-							</div>
-							<input class="button" type="submit" value="SUBMIT">
-						</fieldset>
-					</form>
+
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			// Include the page content template.
+			get_template_part( 'content', 'profile' ); //content-profile.php
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		// End the loop.
+		endwhile;
+		?>
+
 				</div>
+
 <?php	
 	btc_get_sponsor_logos();
 	get_footer();
