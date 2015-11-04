e2f1366993b3238cc9d0d4e3ad654377bd449be6
rpavlovic <rpavlovic@adage.com>
2015-11-04 12:32:57 -0500

    latest

--- wp-content/themes/brooklyntri/content-roster.php
+++ wp-content/themes/brooklyntri/content-roster.php
@@ -87,7 +87,7 @@ unset($categories[0]);
 if ( is_user_logged_in() && !current_user_registered( $post->ID ) ): //!$form_set &&  $form_set = true;
 
 ?>
-										<dd><?php echo gravity_form(1, $display_title=false, $display_description=true, $display_inactive=false, $field_values=null); //, $ajax=true ?></dd>
+										<dd><?php echo gravity_form($post->ID, $display_title=false, $display_description=true, $display_inactive=false, $field_values=null); //, $ajax=true ?></dd>
 <?php endif; ?>
 
 									</dl>
