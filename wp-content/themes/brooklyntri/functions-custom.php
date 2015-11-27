fa59e4f595ea6246a9b3a13a8cced7e5504e0914
rpavlovic <rpavlovic@adage.com>
2015-11-26 19:14:35 -0500

    latest

--- wp-content/themes/brooklyntri/functions-custom.php
+++ wp-content/themes/brooklyntri/functions-custom.php
@@ -1231,8 +1231,7 @@ function count_btc_registrants( $form_id, $event_id) {
 
 function btc_remove_racer() {
 	if (isset($_POST) && isset($_POST['key']) && is_numeric($_POST['key']) && $_SERVER['REQUEST_URI'] == admin_url('admin-ajax.php')) {
-		$_POST['key'] = intval($_POST['key']);
-		GFAPI::delete_entry( $_POST['key'] ;
+		GFAPI::delete_entry( $_POST['key'] );
 
 			echo $_POST['key'];
 
