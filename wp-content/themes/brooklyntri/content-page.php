b16b39e32f873fb4192b2edad4454864196d4aec
rpavlovic <rpavlovic@adage.com>
2015-11-28 10:27:43 -0500

    latest

--- wp-content/themes/brooklyntri/content-page.php
+++ wp-content/themes/brooklyntri/content-page.php
@@ -24,6 +24,9 @@
 						<li>You’ve never been a member at all. Please check out <a href="<?= site_url( '/join' ) ?>">our membership benefits</a> and considering joining the club!</li>
 					</ol>
 				<?php elseif (strstr($_SERVER['REQUEST_URI'], 'login?redirect_to') != false): ?>
+					<p>
+					If you are seeing this message, one of three things is happening:
+					</p>
 					<ol>
 						<li>You haven’t logged in. You must be logged in to view this page.</li>
 						<li>You logged in, but your membership has expired. You must be a member in active standing to view this page. <a href="<?= site_url( '/join' ) ?>">Please renew your membership</a>.</li>
