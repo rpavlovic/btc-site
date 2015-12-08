27c969008a8635035d2bff2e22fd489d8c18c656
Rahmin Pavlovic <rpavlovic@northpointdigital.com>
2015-12-08 10:23:13 -0500

    latest

--- wp-content/themes/brooklyntri/page.php
+++ wp-content/themes/brooklyntri/page.php
@@ -11,10 +11,14 @@
  * @since Brooklyn Tri 1.0
  */
 
-
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
+
+if ( is_user_logged_in() ) {
+	wp_redirect( '/resources/profile' );
+	exit;
+}
 
 get_header();
 
